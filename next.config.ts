d619e88954eefd2041ee3188b8012beeb6d4f34f
decobot <brunowerlang15@gmail.com>
2025-08-25 14:39:26 -0300

    att

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['blog.hylluahusein.com.br'], // adiciona aqui o domínio da imagem
+  },
 };
 
 export default nextConfig;
