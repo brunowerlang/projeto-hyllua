ba96a584bb8da8ecac663433cea912d58db2e3d4
decobot <brunowerlang15@gmail.com>
2025-08-25 14:10:58 -0300

    att

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,26 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="bg-white shadow p-4">
       <nav className="container mx-auto flex justify-between">
         <h1 className="text-xl font-bold">Clínica Hyllua</h1>
         <ul className="flex gap-6">
-          <li><a href="/" className="hover:text-blue-600">Início</a></li>
-          <li><a href="/clinica" className="hover:text-blue-600">Clínica</a></li>
-          <li><a href="/ensino" className="hover:text-blue-600">Ensino</a></li>
-          <li><a href="/hyllua-beauty" className="hover:text-blue-600">Hyllua Beauty</a></li>
-          <li><a href="/blog" className="hover:text-blue-600">Blog</a></li>
+          <li>
+            <Link href="/" className="hover:text-blue-600">Início</Link>
+          </li>
+          <li>
+            <Link href="/clinica" className="hover:text-blue-600">Clínica</Link>
+          </li>
+          <li>
+            <Link href="/ensino" className="hover:text-blue-600">Ensino</Link>
+          </li>
+          <li>
+            <Link href="/hyllua-beauty" className="hover:text-blue-600">Hyllua Beauty</Link>
+          </li>
+          <li>
+            <Link href="/blog" className="hover:text-blue-600">Blog</Link>
+          </li>
         </ul>
       </nav>
     </header>
