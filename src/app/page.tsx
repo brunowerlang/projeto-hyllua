998057965fda3d9d362c91886bc0091a3ce73092
decobot <brunowerlang15@gmail.com>
2025-10-07 10:35:21 -0300

    att

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+
 import Image from "next/image";
+import Link from "next/link";
 // Fetch posts from WordPress API
 async function getLatestPosts() {
   try {
