3b26f34b28b2e7ae7d73324eb35767dd63b335bd
decobot <brunowerlang15@gmail.com>
2025-10-07 11:13:42 -0300

    att

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,7 +58,7 @@ export default async function HomePage() {
         </section>
 
         {/* Seção Logos */}
-        <section className="bg-[#b89c6c] py-1">
+        <section className="bg-[#b89c6c] py-1.5">
           <div className="container max-w-full mx-auto px-1 lg:px-6">
             <div className="flex justify-between items-center overflow-hidden gap-2">
               <Image
@@ -125,7 +125,7 @@ export default async function HomePage() {
                   alt="Profissional da clínica"
                   width={1066}
                   height={1541}
-                  className="rounded-lg w-full h-auto relative -top-12 -right-3 lg:-top-16 lg:-left-8 lg:-right-0 lg:scale-90 scale-110"
+                  className="rounded-lg w-full h-auto relative -top-12 -right-3 lg:-top-28 lg:-left-8 lg:-right-0 lg:scale-85 scale-110"
                 />
               </div>
               
@@ -141,11 +141,11 @@ export default async function HomePage() {
                   
                   {/* Box com ícone */}
                   <div className="flex-1  lg:w-0">
-                    <div className=" rounded-4xl px-0 py-2 lg:py-5 lg:px-13 mb-6 relative lg:w-[25rem]  lg:left-3 border-2 border-[#b89c6c]">
-                      <div className="absolute -top-2 -right-3 lg:-top-9 lg:-right-6 w-9 h-9 lg:w-17 lg:h-17 bg-[#b89c6c] rounded-full flex items-center justify-center ">
-                        <Image src="/images/index/VETOR-SÍMBOLO-SEÇÃO-2-PÁGINA-INICIAL.webp" alt="Logo Hyllua" width={24} height={24} className="lg:w-16 lg:h-16 w-9 h-9 object-contain" />
+                    <div className=" rounded-4xl px-2 py-2 lg:py-5 lg:px-13 mb-6 relative lg:w-[22.5rem]  lg:left-4 border-2 border-[#b89c6c]">
+                      <div className="absolute -top-2 -right-3 lg:-top-9 lg:-right-6 w-9 h-9 lg:w-17 lg:h-17  rounded-full flex items-center justify-center ">
+                        <Image src="/images/index/simbolo-secao-2-inicial.webp" alt="Logo Hyllua" width={24} height={24} className="lg:w-16 lg:h-16 w-9 h-9 object-contain" />
                       </div>
-                      <p className="text-[#b89c6c] lg:text-[1.2rem] lg:leading-[1.7] text-[0.8rem] text-center ">
+                      <p className="text-[#b89c6c] lg:text-[1.3rem] lg:leading-[1.7] text-[0.8rem] text-center ">
                         Transformamos a<br /> experiência estética, <span className="font-semibold italic titulo-stoch">unindo especialidades,</span> ampliando resultados e tocando o que é <span className="font-semibold italic titulo-stoch">visível e essencial.</span>
                       </p>
                     </div>
