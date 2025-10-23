4b18a181401c2cab5fd4499f1c6a22af4d242c6a
decobot <brunowerlang15@gmail.com>
2025-10-23 15:17:26 -0300

    att

--- src/app/clinica/Accordion.tsx
+++ src/app/clinica/Accordion.tsx
@@ -36,7 +36,7 @@ const AccordionAside: React.FC<AccordionAsideProps> = ({ procedimentos }) => {
           <div key={index} className="">
             <button
               onClick={() => toggleAccordion(index)}
-              className={` cursor-pointer w-full flex justify-between items-center lg:px-6 px-4 lg:py-3 py-2 bg-[#353a2c] text-[#e5e6e1] border-t-1 border-[#e5e6e1] font-light tracking-[1px] uppercase lg:text-[13px] text-[12px] ${index === procedimentos.length - 1 ? 'rounded-b-[0.5rem]' : ''}`}
+              className={` cursor-pointer w-full flex justify-between items-center lg:px-6 px-4 lg:py-3 py-2 bg-[#353a2c] text-[#e5e6e1]  border font-light tracking-[1px] uppercase lg:text-[13px] text-[12px] ${index === procedimentos.length - 1 ? 'rounded-b-[0.5rem]' : ''}`}
               type="button"
             >
               {item.titulo}
@@ -46,21 +46,29 @@ const AccordionAside: React.FC<AccordionAsideProps> = ({ procedimentos }) => {
                 {openIndex === index ? "−" : "+"}
               </span>
             </button>
-            <div
-              className={`transition-[max-height,opacity,padding] duration-700 ease-in-out   ${openIndex === index ? "opacity-100 bg-[#abb48d]" : "max-h-0 opacity-0 py-0 bg-[#abb48d]"}`}
-            >
-              {openIndex === index && (
-                <div className="px-0 py-4 grid lg:grid-cols-2 grid-cols-1 gap-x-6 lg:gap-y-4 gap-y-3 text-[#e5e6e1] border-2 lg:text-[13px] text-[12px]">
-                  {item.conteudo.map((linha, i) => (
-                    linha.includes("Exclusivas") ? (
-                      <p key={i} className="font-semibold py-2 bg-[#353a2c] text-[#e5e6e1]  lg:col-span-2 px-3.5 lg:px-6">{linha}</p>
-                    ) : (
-                      <p key={i} className="px-2 lg:px-6 lg:w-[15rem]">{linha}</p>
-                    )
-                  ))}
-                </div>
-              )}
-            </div>
+          <div
+  className={`transition-[max-height] duration-[1200ms] ease-in-out overflow-hidden bg-[#abb48d] ${
+    openIndex === index ? "max-h-[1000px] py-0" : "max-h-0 py-0"
+  }`}
+>
+  <div className="px-0 py-4 grid lg:grid-cols-2 grid-cols-1 gap-x-6 lg:gap-y-4 gap-y-3 text-[#e5e6e1] border-2 lg:text-[13px] text-[12px]">
+    {item.conteudo.map((linha, i) =>
+      linha.includes("Exclusivas") ? (
+        <p
+          key={i}
+          className="font-semibold py-2 bg-[#353a2c] text-[#e5e6e1] lg:col-span-2 px-3.5 lg:px-6"
+        >
+          {linha}
+        </p>
+      ) : (
+        <p key={i} className="px-2 lg:px-6 lg:w-[15rem]">
+          {linha}
+        </p>
+      )
+    )}
+  </div>
+</div>
+
           </div>
         ))}
       </div>
